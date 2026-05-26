46a3954ddbb6fcb00ae6cf5c8e59e16d52481888
SendaoYan <yansendao@hygon.cn>
2026-05-26 17:32:55 +0800

    Commit 2

---
+++ test/jdk/Hello2.java
@@ -0,0 +1,19 @@
+/*
+ * @test
+ */
+import java.io.File;
+import java.nio.file.Path;
+import java.util.Random;
+
+public class Hello2 {
+    public static void main(String[] args) {
+        if (System.getProperty("jtreg") != null)
+            throw new RuntimeException("jtreg");
+        Random r = new Random();
+        if( r.nextInt() % 50 == 0) {
+            throw new RuntimeException("fail!");
+        }
+        System.out.println(r.nextInt(20000) - 10000);
+        throw new RuntimeException("fail");
+    }
+}
